e581fd35fe8639dfd2f58833d70e5c6618a56f23
Justin Rodriguez <justinrodriguez@live.com>
2020-10-07 19:51:46 -0700

    Created function to test if textbox have data

--- ts/main.ts
+++ ts/main.ts
@@ -6,5 +6,27 @@ window.onload = function(){
 }
 
 function main():void {
-    alert("Reg button was clicked");
+    // javascript does not care if we pass in two parameter however typescript does and will give an error
+    isTextPresent("first-name", "First name is required");
+    isTextPresent("last-name", "Last name is required");
+}
+
+/**
+ * Returns true if the textbox with the given id has
+ * some text inside it
+ * @param id The id of the <input type="text"> to validate
+ * @param errMsg The message to display in the sibling span of 
+ * the textbox
+ */
+function isTextPresent(id:string, errMsg:string):boolean {
+    let txtBox = 
+        <HTMLInputElement>document.getElementById("id");
+    let txtBoxValue = txtBox.value;
+    if( txtBoxValue == "" ) {
+        let errSpan = 
+            <HTMLSpanElement>txtBox.nextElementSibling;
+        errSpan.innerText = "errMsg";
+        return false;
+    }
+    return true;
 }
